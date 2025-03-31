4627dbe5f43770fff6923fc99962ac1c0fd95403
Naoki Komazawa <komazawa@ekzm.co.jp>
2025-03-31 13:27:56 +0900

    commit

--- src/mastra/agents/index.ts
+++ src/mastra/agents/index.ts
@@ -1,6 +1,6 @@
-import { google } from '@ai-sdk/google';
-import { Agent } from '@mastra/core/agent';
-import { weatherTool } from '../tools';
+import { google } from '@ai-sdk/google'
+import { Agent } from '@mastra/core/agent'
+import { weatherTool } from '../tools'
 
 export const weatherAgent = new Agent({
   name: 'Weather Agent',
@@ -13,9 +13,10 @@ export const weatherAgent = new Agent({
       - If giving a location with multiple parts (e.g. "New York, NY"), use the most relevant part (e.g. "New York")
       - Include relevant details like humidity, wind conditions, and precipitation
       - Keep responses concise but informative
+      - Response is in Japanese
 
       Use the weatherTool to fetch current weather data.
 `,
-  model: google('gemini-1.5-pro-latest'),
+  model: google('gemini-2.0-flash'),
   tools: { weatherTool },
-});
+})
--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,14 +1,19 @@
-
-import { Mastra } from '@mastra/core/mastra';
-import { createLogger } from '@mastra/core/logger';
-import { weatherWorkflow } from './workflows';
-import { weatherAgent } from './agents';
+import { createLogger } from '@mastra/core/logger'
+import { Mastra } from '@mastra/core/mastra'
+import { VercelDeployer } from '@mastra/deployer-vercel'
+import { weatherAgent } from './agents'
+import { weatherWorkflow } from './workflows'
 
 export const mastra = new Mastra({
+  deployer: new VercelDeployer({
+    teamSlug: process.env.VERCEL_TEAM as string,
+    projectName: process.env.VERCEL_PROJECT as string,
+    token: process.env.VERCEL_TOKEN as string,
+  }),
   workflows: { weatherWorkflow },
   agents: { weatherAgent },
   logger: createLogger({
     name: 'Mastra',
     level: 'info',
   }),
-});
+})
